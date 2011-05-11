e59d011df6bd747a390bf511d649261ae1b26083
Alex Iskander <alexiskander@me.com>
2011-05-10 23:32:08 -0700

    Use SplitView, and make the search view use a custom example view.

--- docs/apps/docs/resources/main_page.js
+++ docs/apps/docs/resources/main_page.js
@@ -4,6 +4,7 @@
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 /*globals Docs */
+sc_require('views/search_result_item_view');
 
 // This page describes the main user interface for your application.  
 Docs.mainPage = SC.Page.design({
@@ -13,92 +14,108 @@ Docs.mainPage = SC.Page.design({
   // load.
   mainPane: SC.MainPane.design({
     layerId: 'mainPane',
-    childViews: 'sidebar detailView'.w(),
+    // childViews: 'sidebar detailView'.w(),
+    childViews: 'splitContainer'.w(),
+
+    sidebar: SC.outlet('splitContainer.sidebar'),
+    detailView: SC.outlet('splitContainer.detailView'),
     
-    sidebar: SC.View.design({
-      layout: { top:0, left:0, bottom:0, width:190 },
-      childViews: 'search classList searchResultsList'.w(),
+    splitContainer: SC.SplitView.design({
+      childViews: 'sidebar detailView'.w(),
 
-      search: SC.View.design({
-        layout: { top:0, left:0, height:35, width:190 },
-        classNames: 'search'.w(),
-        childViews: 'searchTextField'.w(),
+      sidebar: SC.View.design(SC.SplitChild, {
+        layout: { top:0, bottom:0},
+        size: 190,
 
-        searchTextField: SC.TextFieldView.design({
-          hint: 'Search For Symbol',
-          valueBinding: 'Docs.searchController.searchQuery'
-        })
-      }),
+        childViews: 'search classList searchResultsList'.w(),
 
-      classList: SC.ScrollView.design({
-        layout: { top:35, left:0, bottom:0, width:190 },
-        isVisibleBinding: SC.Binding.not('Docs.searchController.isSearching'),
+        search: SC.View.design({
+          layout: { top:0, left:0, height:35, right: 0 },
+          classNames: 'search'.w(),
+          childViews: 'searchTextField'.w(),
 
-        contentView: Docs.MasterListView.design({
-          contentBinding: 'Docs.classesController.arrangedObjects',
-          selectionBinding: 'Docs.classesController.selection',
-          showAlternatingRows: YES,
-          contentValueKey: 'displayName'
-        })
-      }),
-
-      searchResultsList: SC.ScrollView.design({
-        layout: { top:35, left:0, bottom:0, width:190 },
-        isVisibleBinding: 'Docs.searchController.isSearching',
-
-        contentView: Docs.MasterListView.design({
-          contentBinding: 'Docs.searchController.arrangedObjects',
-          selectionBinding: 'Docs.searchController.selection',
-          showAlternatingRows: YES,
-          contentValueKey: 'displayName'
-        })
-      })
-    }),
-
-    detailView: SC.View.design({
-      layout: { top:0, left:190, bottom:0, right:0 },
-      childViews: 'toolbar workspace'.w(),
-
-      toolbar: SC.View.design({
-        layout: { top:0, left:0, right:0, height:50 },
-        classNames: 'class-header'.w(),
-        childViews: 'className'.w(),
-
-        className: SC.LabelView.design({
-          layout: { centerY:0, left:10, right:0, height:27 },
-          classNames: 'class-name'.w(),
-          valueBinding: 'Docs.selectedClassController.displayName',
-          controlSize: SC.LARGE_CONTROL_SIZE
-        })
-      }),
-
-      workspace: SC.View.design({
-        layout: { top:50, left:0, bottom:0, right:0 },
-        classNames: 'workspace'.w(),
-        childViews: 'symbolList classDetails'.w(),
-
-        symbolList: SC.ScrollView.design({
-          layout: { top:0, left:0, bottom:0, width:190 },
-          contentView: Docs.DetailListView.design({
-            classNames: 'symbol-list'.w(),
-            contentBinding: 'Docs.selectedClassController.symbols',
-            selectionBinding: 'Docs.selectedClassController.symbolSelection',
-            contentValueKey: 'name',
-            contentIconKey: 'icon',
-
-            exampleView: SC.ListItemView.extend({
-              hasContentIcon: YES
-            })
-          }) 
+          searchTextField: SC.TextFieldView.design({
+            hint: 'Search For Symbol',
+            valueBinding: 'Docs.searchController.searchQuery'
+          })
         }),
 
-        classDetails: SC.TemplateView.design({
-          classNames: 'class-detail'.w(),
-          isTextSelectable: YES,
-          contentBinding: 'Docs.selectedClassController.content',
-          templateName: 'details'
+        classList: SC.ScrollView.design({
+          layout: { top:35, left:0, bottom:0, right: 0 },
+          isVisibleBinding: SC.Binding.not('Docs.searchController.isSearching'),
+
+          contentView: Docs.MasterListView.design({
+            contentBinding: 'Docs.classesController.arrangedObjects',
+            selectionBinding: 'Docs.classesController.selection',
+            showAlternatingRows: YES,
+            contentValueKey: 'displayName'
+          })
+        }),
+
+        searchResultsList: SC.ScrollView.design({
+          layout: { top:35, left:0, bottom:0, right: 0 },
+          isVisibleBinding: 'Docs.searchController.isSearching',
+
+          // the custom example view supports fast path, so we can use it and the
+          // trackpad momentum scrolling works!
+          contentView: Docs.MasterListView.design(SC.CollectionFastPath, {
+            contentBinding: 'Docs.searchController.arrangedObjects',
+            selectionBinding: 'Docs.searchController.selection',
+            showAlternatingRows: YES,
+
+            exampleView: Docs.SearchResultItemView
+          })
+        })
+      }),
+
+      detailView: SC.View.design(SC.SplitChild, {
+        layout: { top:0, bottom:0 },
+        childViews: 'toolbar workspace'.w(),
+
+        autoResizeStyle: SC.RESIZE_AUTOMATIC,
+
+        toolbar: SC.View.design({
+          layout: { top:0, left:0, right:0, height:50 },
+          classNames: 'class-header'.w(),
+          childViews: 'className'.w(),
+
+          className: SC.LabelView.design({
+            layout: { centerY:0, left:10, right:0, height:27 },
+            classNames: 'class-name'.w(),
+            valueBinding: 'Docs.selectedClassController.displayName',
+            controlSize: SC.LARGE_CONTROL_SIZE
+          })
+        }),
+
+        workspace: SC.View.design({
+          layout: { top:50, left:0, bottom:0, right:0 },
+          classNames: 'workspace'.w(),
+          childViews: 'symbolList classDetails'.w(),
+
+          symbolList: SC.ScrollView.design({
+            layout: { top:0, left:0, bottom:0, width:190 },
+            contentView: Docs.DetailListView.design({
+              classNames: 'symbol-list'.w(),
+              contentBinding: 'Docs.selectedClassController.symbols',
+              selectionBinding: 'Docs.selectedClassController.symbolSelection',
+              contentValueKey: 'name',
+              contentIconKey: 'icon',
+
+              exampleView: SC.ListItemView.extend({
+                hasContentIcon: YES
+              })
+            }) 
+          }),
+
+          classDetails: SC.TemplateView.design({
+            classNames: 'class-detail'.w(),
+            isTextSelectable: YES,
+            contentBinding: 'Docs.selectedClassController.content',
+            templateName: 'details'
+          })
         })
       })
+   
     })
   })
 });
--- docs/apps/docs/views/search_result_item_view.js
+++ docs/apps/docs/views/search_result_item_view.js
@@ -4,4 +4,11 @@
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 
+Docs.SearchResultItemView = SC.View.design({
+  layerIsCacheable: YES,
+  isPoolable: YES,
 
+  render: function(context, firstTime) {
+    context.push(this.get('content').get('searchNameMarkup'));
+  }
+});
